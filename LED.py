b6e290a3b89bb9e9a46bcba083adcf249fad322d
jake <jakeburden5@gmail.com>
2018-12-14 12:39:10 +0000

    first commit

---
+++ LED.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(23,GPIO.OUT)
+print "LED 1 on"
+GPIO.output(23,GPIO.HIGH)
+time.sleep(1)
+print "LED off"
+GPIO.output(23,GPIO.LOW)
+
