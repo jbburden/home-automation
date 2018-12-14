f0f5f24ef2653687a55a8620ca8763efe4a2db44
jake <jakeburden5@gmail.com>
2018-12-14 12:56:47 +0000

    2 lights

--- LED.py
+++ LED.py
@@ -2,10 +2,17 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
 GPIO.setup(23,GPIO.OUT)
 print "LED 1 on"
 GPIO.output(23,GPIO.HIGH)
-time.sleep(1)
-print "LED off"
+time.sleep(10)
+print "LED 1 off"
 GPIO.output(23,GPIO.LOW)
 
+GPIO.setup(24,GPIO.OUT)
+print "LED 2 on"
+GPIO.output(24,GPIO.HIGH)
+time.sleep(10)
+print "LED 2 off"
+GPIO.output(24,GPIO.LOW)
